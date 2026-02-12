48e5a0940ca2c86404f15d726e243d8e9e0e0008
Kodz-boss <kodiokeke0@gmail.com>
2026-02-12 11:02:25 +0100

    update

--- if-statements.php
+++ if-statements.php
@@ -1,10 +1,60 @@
 <?php 
-    $age = "17";
+    // $age = "17";
 
-    if($age >= 18){
-        echo " Your are old enough";
-    } else{
-        echo "too young";
-    }
+    // if($age >= 18){
+    //     echo " Your are old enough";
+    // } else{
+    //     echo "too young";
+    // }
 
-    ?>
+
+
+
+//== is equal to
+//!= is not equal to
+//< less than
+//> greater than
+//>= greater than or equal to
+//<= less than or equal to
+//&& and
+//|| or
+//! not
+
+
+
+
+
+
+
+
+
+
+//ifelse to check multiple statements
+
+
+
+// $score = 35;
+
+// if($score >= 80){
+//     echo "You got an A";
+// } else if($score >= 70){
+//     echo "You got a B";
+// } else if($score >= 60){
+//     echo "You got a C";
+// } else {
+//     echo "You failed";
+// }
+
+
+
+//combining conditons
+// $age = 25;
+// $hasID = false;
+
+// if($age >= 18 && $hasID == true){
+//     echo "Welcome!, You may enter.";
+// }else{
+//     echo "Sorry, you may not enter.";
+// }
+
+?>
