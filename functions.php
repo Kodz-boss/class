fe10bc4e6f217386f14983fd316ca43eea6133ac
Kodz-boss <kodiokeke0@gmail.com>
2026-02-19 10:30:06 +0100

    update

--- functions.php
+++ functions.php
@@ -20,22 +20,41 @@
 
     //Built in functions for php
     //1. String functions{This lets you work and manipulate text}
-    $text = "Hello WOrld, Welcome  to php";
+    // $text = "Hello WOrld, Welcome  to php";
 
-    //Count the number of characters - strlen
-    echo strlen($text) . "<br>";
+    // //Count the number of characters - strlen
+    // echo strlen($text) . "<br>";
 
-    //convert to uppercase - strtoupper
-    echo strtoupper($text) . "<br>";
+    // //convert to uppercase - strtoupper
+    // echo strtoupper($text) . "<br>";
 
-    //convert to lowercase - strtolower
-    echo strtolower($text) . "<br>";
+    // //convert to lowercase - strtolower
+    // echo strtolower($text) . "<br>";
 
-    //reverse the text -strrev
-    echo strrev($text) . "<br>";
+    // //reverse the text -strrev
+    // echo strrev($text) . "<br>";
 
-    //count the number of words - str_word_count
-    echo str_word_count($text) . "<br>";
+    // //count the number of words - str_word_count
+    // echo str_word_count($text) . "<br>";
 
-    //Replace a with another - str_replace
-    echo str_replace("php", "Programming", $text) ."<br>";
+    // //Replace a with another - str_replace
+    // echo str_replace("php", "Programming", $text) ."<br>";
+
+
+    //number functions
+    // $number = 7;
+
+    // //round to the nearest whole number
+    // echo round($number) . "<br>";
+
+    // //round DOWN always
+    // echo floor($number) . "<br>";
+
+    // //round UP always
+    // echo ceil($number) . "<br>";
+
+    // //Absolute values (Removes negative sign)
+    // echo abs(-15) . "<br>";
+
+    // //Generate a random number between 1 and 100
+    // echo rand(1, 100) . "<br>";
